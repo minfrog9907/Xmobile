b6c30313318fd6bd80054817547eb317974f46c8
DESKTOP-CHGBI3E\HP <willtriti03@naver.com>
2017-09-26 16:25:29 +0900

    cpp

---
+++ Xmobile/cpp/native-lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 2017-09-26.
+//
+
+#include "native-lib.h"
---
+++ Xmobile/cpp/native-lib.h
@@ -0,0 +1,8 @@
+//
+// Created by HP on 2017-09-26.
+//
+
+#ifndef XMOBILE_NATIVE_LIB_H
+#define XMOBILE_NATIVE_LIB_H
+
+#endif //XMOBILE_NATIVE_LIB_H

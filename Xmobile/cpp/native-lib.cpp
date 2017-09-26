869ac401f6f12ecb77376218440b00e47e50b819
DESKTOP-CHGBI3E\HP <willtriti03@naver.com>
2017-09-26 16:34:49 +0900

    얍

--- Xmobile/cpp/native-lib.cpp
+++ Xmobile/cpp/native-lib.cpp
@@ -1,5 +1,11 @@
-//
-// Created by HP on 2017-09-26.
-//
+#include <jni.h>
+#include <string>
 
-#include "native-lib.h"
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_hp_myapplication_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}

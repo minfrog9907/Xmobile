6696ff8587f7e6aace4fdcf79bbff587ece38f67
DESKTOP-CHGBI3E\HP <willtriti03@naver.com>
2017-09-28 10:30:02 +0900

    이미지 변환 성공

--- Xmoblie/app/src/main/cpp/native-lib.cpp
+++ Xmoblie/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,46 @@
 #include <jni.h>
-#include <string>
+#include <opencv2/opencv.hpp>
+#include <android/asset_manager_jni.h>
+#include <android/log.h>
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_example_hp_xmoblie_MainActivity_stringFromJNI(
+using namespace cv;
+using namespace std;
+
+extern "C" {
+
+JNIEXPORT void JNICALL
+Java_com_example_hp_xmoblie_Activity_ImageGrayScaleActivity_loadImage(
         JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject,
+        jstring imageFileName,
+        jlong addrImage) {
+
+    Mat &img_input = *(Mat *) addrImage;
+
+    const char *nativeFileNameString = env->GetStringUTFChars(imageFileName, JNI_FALSE);
+
+    string baseDir("/storage/emulated/0/");
+    baseDir.append(nativeFileNameString);
+    const char *pathDir = baseDir.c_str();
+
+    img_input = imread(pathDir, IMREAD_COLOR);
+
+}
+
+JNIEXPORT void JNICALL
+Java_com_example_hp_xmoblie_Activity_ImageGrayScaleActivity_imageprocessing(
+        JNIEnv *env,
+        jobject,
+        jlong addrInputImage,
+        jlong addrOutputImage) {
+
+    Mat &img_input = *(Mat *) addrInputImage;
+    Mat &img_output = *(Mat *) addrOutputImage;
+
+    cvtColor( img_input, img_input, CV_BGR2RGB);
+    cvtColor( img_input, img_output, CV_RGB2GRAY);
+    blur( img_output, img_output, Size(5,5) );
+    Canny( img_output, img_output, 50, 150, 5 );
+}
+
 }

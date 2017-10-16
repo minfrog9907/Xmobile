be194e876196ccef4557ca0ffbaab560902dec9d
DESKTOP-CHGBI3E\HP <willtriti03@naver.com>
2017-10-16 13:55:44 +0900

    login

--- Xmoblie/app/src/main/java/com/example/hp/xmoblie/Activity/SplashActivity.java
+++ Xmoblie/app/src/main/java/com/example/hp/xmoblie/Activity/SplashActivity.java
@@ -14,8 +14,17 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.hp.xmoblie.Items.LoginItem;
 import com.example.hp.xmoblie.R;
+import com.example.hp.xmoblie.Service.ApiClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by triti on 2017-07-13.
@@ -24,7 +33,7 @@ import com.example.hp.xmoblie.R;
 public class SplashActivity extends BaseActivity {
     private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1000;
     private final int SPLASH_DISPLAY_LENGTH = 1500;
-    String[] permissions = new String[3];
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -33,13 +42,15 @@ public class SplashActivity extends BaseActivity {
         int internetPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
         int storagePermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         int cameraPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int readStatePermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
 
 
         if (internetPermissionCheck == PackageManager.PERMISSION_DENIED
                 ||storagePermissionCheck == PackageManager.PERMISSION_DENIED
-                ||cameraPermissionCheck == PackageManager.PERMISSION_DENIED) {
+                ||cameraPermissionCheck == PackageManager.PERMISSION_DENIED
+                ||readStatePermissionCheck ==PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.INTERNET},
+                    new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.INTERNET,Manifest.permission.READ_PHONE_STATE},
                     MY_PERMISSIONS_REQUEST_READ_CONTACTS);
 
         }
@@ -48,7 +59,8 @@ public class SplashActivity extends BaseActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_DENIED
                 && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED
                 && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_DENIED) {
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_DENIED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_DENIED) {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
@@ -102,9 +114,49 @@ public class SplashActivity extends BaseActivity {
         finish();
         SharedPreferences autoLogin = getSharedPreferences("autoLogin",0);
         if(autoLogin.getBoolean("autoLogin",false)){
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));// 액티비티 종료
-        }else{
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));// 액티비티 종료
+        }else{
+            TelephonyManager mgr = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+
+            ApiClient apiClient = ApiClient.service;
+
+            String deviceid = "testDevice";//mgr.getDeviceId();
+            String userid="",password="";
+            autoLogin.getString("id",userid);
+            autoLogin.getString("password",password);
+            Call<LoginItem> call = apiClient.repoContributors(userid, password, deviceid);
+            call.enqueue(new Callback<LoginItem>() {
+                @Override
+                public void onResponse(Call<LoginItem> call,
+                                       Response<LoginItem> response) {
+                    switch (response.body().getStatus()) {
+                        case 0:
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.putExtra("username", response.body().getUsername());
+                            intent.putExtra("token", response.body().getToken());
+                            intent.putExtra("privilege", response.body().getPrivilege());
+                            startActivity(intent);
+                            break;
+                        case 1:
+                            Toast.makeText(getApplicationContext(), "잘못된 아이디 또는 비밀번호입니다.", Toast.LENGTH_LONG).show();
+                            break;
+                        case 2:
+                            Toast.makeText(getApplicationContext(), "잠긴 계정입니다.", Toast.LENGTH_LONG).show();
+                            break;
+                        default:
+                            Toast.makeText(getApplicationContext(), "알 수 없는 오류  " + response.body().getStatus(), Toast.LENGTH_LONG).show();
+                            break;
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<LoginItem> call, Throwable t) {
+                    Log.e("jsonResponse", "빼애애앵ㄱ");
+
+                }
+
+
+            });
         }
         //startActivity(new Intent(getApplicationContext(), CheerPopUp.class));// 액티비티 종료
 
---
+++ Xmoblie/app/src/main/java/com/example/hp/xmoblie/Service/ApiClient.java
@@ -0,0 +1,41 @@
+package com.example.hp.xmoblie.Service;
+
+/**
+ * Created by HP on 2017-10-16.
+ */
+
+import com.example.hp.xmoblie.Items.LoginItem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by jungjune on 2016-07-22.
+ */
+public interface ApiClient {
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://xmobile2.lfconfig.xyz")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    ApiClient service = retrofit.create(ApiClient.class);
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<LoginItem> repoContributors(
+            @Field("userid") String userid
+            , @Field("password") String password
+            , @Field("deviceid") String deviceid
+
+    );
+}
+
+
